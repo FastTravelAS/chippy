ea3bf7cb298a256f366fe73ca58a93cb5774705c
Martin Ulleberg <martin.ulleberg@gmail.com>
2023-03-20 23:47:26 +0100

    Handle malformed messages

--- lib/chippy/connection.rb
+++ lib/chippy/connection.rb
@@ -18,10 +18,13 @@ module Chippy
       length_of_data = header.message_length
       data = client.read(length_of_data)
 
+      # Calculate the remaining data length
+      remaining_data_length = length_of_data - data.bytesize
+
       body = Message::Body.parse(data)
       message = Message.new(header: header, body: body)
 
-      raise Chippy::MessageError, "Status: #{message.status}, id: #{message.message_id}" unless message.ok?
+      raise Chippy::MalformedMessageError.new(remaining_data_length: remaining_data_length) unless message.ok?
 
       message
     end
@@ -31,5 +34,12 @@ module Chippy
     end
 
     delegate :close, to: :client
+
+    def discard_remaining_data(remaining_data_length)
+      return unless remaining_data_length
+
+      # Read and discard the remaining data
+      client.read(remaining_data_length)
+    end
   end
 end
